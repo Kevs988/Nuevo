3626c08b595d4ebbbbd8d52a447e1e047e3f15eb
Kevs988 <122298896+Kevs988@users.noreply.github.com>
2023-02-02 01:16:01 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function fecha(){
+    document.getElementById('info').innerHTML = "23-09-1988";
+    
+  }
+  function Fecha(){
+    document.getElementById('info').innerHTML = "Fecha de nacimiento";
+  }
+  
+  function nac(){
+    document.getElementById('info1').innerHTML = "Argentina/34028982";
+    
+  }
+  function Nac(){
+    document.getElementById('info1').innerHTML = "Nacionalidad/D.N.I";
+  }
+  function est(){
+    document.getElementById('info2').innerHTML = "Soltero";
+    
+  }
+  function Est(){
+    document.getElementById('info2').innerHTML = "Estado civil";
+  }
